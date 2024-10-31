db3326b34dbf48a9408e6088ac0f89b504119190
Mark <61754061+mrkgolban@users.noreply.github.com>
2024-10-31 22:30:23 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+int my_strlen(char* str)
+{
+    int s = 0;
+    while (isalpha(*(str + s)) || isdigit(*(str + s)))
+    {
+        s += 1;
+    }
+    return s;
+}
+
+// int my_strstr(char* haystack, char* needle)
+// {
+
+// }
+
+void my_strcpy(char *source, char* target)
+{
+    for (int i = 0; i < my_strlen(source); i++)
+    {
+        *target = *source;
+    }
+}
+
+int my_strstr(char* haystack, char* needle)
+{
+    int i, j = 0;
+    int res = -1;
+    bool flag = false;
+    for (int i = 0; i < my_strlen(haystack); i++)
+    {
+        if (*(haystack + i) == *(needle))
+        {
+        flag = true;
+            for (int j = 0; j < my_strlen(needle); j++)
+            {
+                if (*(haystack + i + j) != *(needle + j))
+                {
+                    flag = false;
+                }
+            }
+        }
+        if (flag)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    char* string = "abcdef123";
+    char* string2 = "1";
+    printf("%d\n", my_strlen(string));
+    string2 = "0";
+    printf("%s\n%s\n", string2, string);
+    printf("%d\n", my_strstr(string, string2));
+    
+}
